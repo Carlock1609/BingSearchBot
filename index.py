3aa13c073255a48fd058f83319f9699f80ee04a6
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-11-13 23:48:14 -0800

    Finished loop layout, browser remains open | Need to clear or refresh page, element gets stale

--- index.py
+++ index.py
@@ -1,12 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-random_word = "Hello"
+import string
+import random
 
+counter = 0
+random_word = list(string.ascii_lowercase)
 
-driver = webdriver.Firefox()
-driver.get("https://www.Bing.com")
+driver = webdriver.Firefox() # Opens firefox
+driver.maximize_window() # Maximizes window
+driver.implicitly_wait(10) # Gives the page time to load before initiating
+driver.get("https://www.Bing.com") # searches
 
-element = driver.find_element_by_id("sb-form-q")
-element.send_keys(random_word)
-element.send_keys(Keys.RETURN)
-element.close()
+while counter <= 100: # The word and keys are in the loop so the browser only opens once
+    random.shuffle(random_word)
+    word = "".join(random_word)
+
+    element = driver.find_element_by_id("sb_form_q")
+    element.clear()
+    element.send_keys(random_word)
+    element.send_keys(Keys.RETURN)
+    
