57e77302f0b8108bda82ff19adf4496f571cc584
Jon <53983746+Carlock1609@users.noreply.github.com>
2019-11-16 16:01:34 -0800

    Bot completed

---
+++ README.md
@@ -0,0 +1,17 @@
+"INFO" 
+Bing Search Bot
+
+Project used selenium
+
+practice with drivers
+Bots
+
+This is a bot that completes the bing searchs challenge for microsoft points. 
+
+When executed it searchs as many times as needed then shuts down.
+
+-
+TODOS
+
+trying to find a way that it runs automatically everyday.
+
--- index.py
+++ index.py
@@ -2,21 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import string
 import random
+import time
 
 counter = 0
 random_word = list(string.ascii_lowercase)
 
 driver = webdriver.Firefox() # Opens firefox
-driver.maximize_window() # Maximizes window
+driver.set_window_size(600,400) # Small window -> fasting loading
 driver.implicitly_wait(10) # Gives the page time to load before initiating
-driver.get("https://www.Bing.com") # searches
+driver.get("https://www.Bing.com") # grabs the url
 
 while counter <= 100: # The word and keys are in the loop so the browser only opens once
-    random.shuffle(random_word)
+    random.shuffle(random_word) # random word
     word = "".join(random_word)
 
-    element = driver.find_element_by_id("sb_form_q")
-    element.clear()
-    element.send_keys(random_word)
-    element.send_keys(Keys.RETURN)
+    searchbar = driver.find_element_by_id("sb_form_q") #looking for the id for the searchbar
+
+    searchbar.send_keys(Keys.CONTROL,'a') # clears searchbar
+    searchbar.send_keys(Keys.DELETE) 
+    time.sleep(1) 
+    searchbar.send_keys(random_word) # inputs word
+    searchbar.send_keys(Keys.RETURN) # submits
+    time.sleep(1) # Giving the script time to load for some reason makes it work
     
